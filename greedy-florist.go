99e0abdbaff56eac84ed8681a423c7b9ea61cc76
John McKee <jmckee3@mac.com>
2019-12-16 17:03:46 -0800

    working version

--- greedy-florist.go
+++ greedy-florist.go
@@ -1,3 +1,31 @@
 package main
 
+import "sort"
+
+type FlowerCosts []int32
+
+func (a FlowerCosts) Len() int           { return len(a) }
+func (a FlowerCosts) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a FlowerCosts) Less(i, j int) bool { return a[i] < a[j] }
+
+func getMinimumCost(k int32, c []int32) int32 {
+	var numSold int32
+	var cost int32
+	cLength := int32(len(c))
+
+	flowerCosts := FlowerCosts(c)
+	sort.Sort(flowerCosts)
+
+	for i := cLength - 1; i >= 0; i-- {
+		cost += purchaseFlower(k, flowerCosts[i], numSold)
+		numSold++
+	}
+	return cost
+}
+
+func purchaseFlower(k int32, price int32, numSold int32) int32 {
+
+	return (numSold/k + 1) * price
+}
+
 func main() {}
--- spec-greedy-florist_test.go
+++ spec-greedy-florist_test.go
@@ -2,6 +2,46 @@ package main
 
 import "testing"
 
-func TestGreedyFlorist(t *testing.T) {
+func TestGreedyFlorist1(t *testing.T) {
+	k := int32(3)
+	c := []int32{2, 5, 6}
 
+	minCost := getMinimumCost(k, c)
+
+	if minCost != 13 {
+		t.Errorf("got %d instead of 13", minCost)
+	}
+}
+
+func TestGreedyFlorist2(t *testing.T) {
+	k := int32(2)
+	c := []int32{2, 5, 6}
+
+	minCost := getMinimumCost(k, c)
+
+	if minCost != 15 {
+		t.Errorf("got %d instead of 15", minCost)
+	}
+}
+
+func TestGreedyFlorist3(t *testing.T) {
+	k := int32(3)
+	c := []int32{1, 3, 5, 7, 9}
+
+	minCost := getMinimumCost(k, c)
+
+	if minCost != 29 {
+		t.Errorf("got %d instead of 29", minCost)
+	}
+}
+
+func TestGreedyFlorist4(t *testing.T) {
+	k := int32(3)
+	c := []int32{390225, 426456, 688267, 800389, 990107, 439248, 240638, 15991, 874479, 568754, 729927, 980985, 132244, 488186, 5037, 721765, 251885, 28458, 23710, 281490, 30935, 897665, 768945, 337228, 533277, 959855, 927447, 941485, 24242, 684459, 312855, 716170, 512600, 608266, 779912, 950103, 211756, 665028, 642996, 262173, 789020, 932421, 390745, 433434, 350262, 463568, 668809, 305781, 815771, 550800}
+
+	minCost := getMinimumCost(k, c)
+
+	if minCost != 163578911 {
+		t.Errorf("got %d instead of 163578911", minCost)
+	}
 }
